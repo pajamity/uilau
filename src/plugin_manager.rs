e8a224cb31fadf949e8a548e770a8e8688b278d9
pajamity <pajamity@tutanota.com>
2020-08-29 09:18:32 +0900

    Added plugin manager

---
+++ src/plugin_manager.rs
@@ -0,0 +1,31 @@
+extern crate gstreamer_video as gst_video;
+use gst_video::prelude::*;
+
+extern crate gstreamer_editing_services as ges;
+use ges::prelude::*;
+
+extern crate libloading;
+
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use super::object::{ObjectContent, Object};
+use super::layer::Layer;
+use crate::util;
+
+pub trait Plugin {
+
+}
+
+pub struct PluginManager {
+  pub plugins: Arc<Mutex<Vec<Arc<Mutex<Plugin>>>>>,
+}
+
+impl PluginManager {
+  // find available plugins
+
+  // load a plugin
+
+  // 
+}
